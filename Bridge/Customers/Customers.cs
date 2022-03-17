c780bbe20b8d8ad697b7bd82b9eba6311a238016
József Krizsán <fysikko@gmail.com>
2022-03-17 08:45:15 +0100

    Add Bridge

---
+++ Bridge/Customers/Customers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bridge.Customers
+{
+    /// <summary>
+    /// The 'RefinedAbstraction' class
+    /// </summary>
+    public class Customers : CustomersBase
+    {
+        public override void ShowAll()
+        {
+            Console.WriteLine();
+            Console.WriteLine("------------------------");
+            base.ShowAll();
+            Console.WriteLine("------------------------");
+        }
+    }
+}
