18777864f868f4278b313424d0ec24aa4ead8052
József Krizsán <fysikko@gmail.com>
2022-03-17 07:27:07 +0100

    Add Singleton

---
+++ Singleton/LoadBalancer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    public class LoadBalancer
+    {
+        private static LoadBalancer instance;
+        private static readonly object locker = new object();
+
+        private readonly List<string> _servers = new List<string>();
+        private readonly Random random = new Random();
+
+        protected LoadBalancer()
+        {
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+            _servers.Add("ServerIV");
+            _servers.Add("ServerV");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            // Support multithreaded applications through
+            // 'Double checked locking' pattern which (once
+            // the instance exists) avoids locking each
+            // time the method is invoked
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LoadBalancer();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public string GetServer()
+            => _servers[random.Next(_servers.Count)].ToString();
+    }
+}
