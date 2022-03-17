d42dd1bacaacadc7336e29cc667350c041aa6787
József Krizsán <fysikko@gmail.com>
2022-03-17 09:05:52 +0100

    Add Composite

---
+++ Composite/DrawingElement.cs
@@ -0,0 +1,21 @@
+﻿namespace Composite
+{
+    /// <summary>
+    /// The 'Component' Treenode
+    /// </summary>
+    public abstract class DrawingElement
+    {
+        protected string _name;
+
+        public DrawingElement(string name)
+        {
+            _name = name;
+        }
+
+        public abstract void Add(DrawingElement d);
+
+        public abstract void Remove(DrawingElement d);
+
+        public abstract void Display(int indent);
+    }
+}
