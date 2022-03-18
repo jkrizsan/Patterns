f9ea0f459096476a47f374ba1271b12a7a366daa
József Krizsán <fysikko@gmail.com>
2022-03-18 09:30:26 +0100

    Add Interpreter

---
+++ Interpreter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Interpreter.Expressions;
+using System.Collections.Generic;
+
+namespace Interpreter
+{
+    /// <summary>
+    /// Interpreter Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string roman = "MCMXXVIII";
+            Context context = new Context(roman);
+
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TenExpression());
+            tree.Add(new OneExpression());
+
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.WriteLine("{0} = {1}",
+                roman, context.Output);
+
+            Console.ReadKey();
+        }
+    }
+}
