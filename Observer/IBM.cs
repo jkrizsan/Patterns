c0daafbe1124b05f4f08c48740c0da7d09a7c409
József Krizsán <fysikko@gmail.com>
2022-03-18 13:27:01 +0100

    Add Observer

---
+++ Observer/IBM.cs
@@ -0,0 +1,13 @@
+﻿namespace Observer
+{
+    /// <summary>
+    /// The 'ConcreteSubject' class
+    /// </summary>
+    public class IBM : Stock
+    {
+        public IBM(string symbol, double price)
+            : base(symbol, price)
+        {
+        }
+    }
+}
