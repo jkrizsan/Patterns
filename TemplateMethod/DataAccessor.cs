d16a225ce855f558cbf31114456e15bba6454125
József Krizsán <fysikko@gmail.com>
2022-03-18 14:34:54 +0100

    Add TemplateMethod

---
+++ TemplateMethod/DataAccessor.cs
@@ -0,0 +1,24 @@
+﻿namespace TemplateMethod
+{
+    /// <summary>
+    /// The 'AbstractClass' abstract class
+    /// </summary>
+    public abstract class DataAccessor
+    {
+        public abstract void Connect();
+
+        public abstract void Select();
+
+        public abstract void Process(int top);
+
+        public abstract void Disconnect();
+
+        public void Run(int top)
+        {
+            Connect();
+            Select();
+            Process(top);
+            Disconnect();
+        }
+    }
+}
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    /// <summary>
+    /// Template Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataAccessor categories = new Categories();
+            categories.Run(5);
+
+            DataAccessor products = new Products();
+            products.Run(3);
+
+            Console.ReadKey();
+        }
+    }
+}
